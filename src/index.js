a26b8743880d59274b6d26d66dcc6b613807da10
prettyboyheaven <jemzetrim@gmail.com>
2017-11-28 22:15:50 +0300

    wip: share to social networks

--- src/index.js
+++ src/index.js
@@ -3,5 +3,29 @@ import objectFitImages from 'object-fit-images';
 
 
 window.addEventListener('load', () => {
-  objectFitImages();
+    objectFitImages();
+
+
+    let button = document.getElementsByClassName('share')[0];
+
+    button.addEventListener('click', () => {
+            share();
+        }
+    );
+
+    const openWindow = (url) => {
+        window.open(
+            `http://twitter.com/share?text&url=${url}`,
+            'Поделиться',
+            'left=100,top=100,width=920,height=500,resizable=yes'
+        )
+    };
+
+    const share = () => {
+        const url = window.location.href;
+        openWindow(url);
+    };
+
+
 });
+
