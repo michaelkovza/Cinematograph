56eb1124188a53727f349e1fb266da5d22510efd
prettyboyheaven <jemzetrim@gmail.com>
2018-04-09 20:18:27 +0300

    feat: refactor infiniteScroll

--- src/js/infiniteScroll.js
+++ src/js/infiniteScroll.js
@@ -3,36 +3,54 @@ import $ from 'jquery';
 let reviewsButton = document.getElementsByClassName('js-reviews-button')[0];
 let articlesButton = document.getElementsByClassName('js-articles-button')[0];
 
-let type = 'default';
-let loadUrl;
+// везде тип default, если это не страница журнал
+
+const types = {
+  default: 'default',
+  articles: 'articles',
+  reviews: 'reviews'
+};
+
+let type = types.default;
 
 if (reviewsButton !== undefined && articlesButton !== undefined) {
-    type = 'articles';
+    type = types.articles;
 
     reviewsButton.addEventListener('click', () => {
-        type = 'reviews';
+        type = types.reviews;
     });
 
     articlesButton.addEventListener('click', () => {
-        type = 'articles';
+        type = types.articles;
     });
 }
 
+
 const infiniteScroll = () => {
 
-
     let currentUrl = window.location.href;
-    let countDefault = 1;
-    let countReviews = 1;
-    let countArticles = 1;
 
     if (!window.scrollData) {
         return
     }
 
-    let navNumDefault;
-    let navNumReviews;
-    let navNumArticles;
+    let dataPagination = {
+        [types.default]: {
+            navNum: window.scrollData.default.loadSett.navNum,
+            endPage: window.scrollData.default.endPage,
+            count: 1
+        },
+        [types.articles]: {
+            navNum: window.scrollData.articles.loadSett.navNum,
+            endPage: window.scrollData.articles.endPage,
+            count: 1
+        },
+        [types.reviews]: {
+            navNum: window.scrollData.reviews.loadSett.navNum,
+            endPage: window.scrollData.reviews.endPage,
+            count: 1
+        }
+    };
 
     let infinityContainer = document.getElementsByClassName('js-infinity-content')[0];
 
@@ -43,9 +61,7 @@ const infiniteScroll = () => {
     let loader = document.getElementsByClassName('js-loader')[0];
     let loaderHiddenClass = 'loader--hidden';
 
-
     const getData = () => {
-
 
             let data = new FormData();
             data.append("AJAX", "Y");
@@ -54,10 +70,10 @@ const infiniteScroll = () => {
 
             let xhr = new XMLHttpRequest();
 
-            if (type === 'reviews' ) {
+            /*if (type === 'reviews' ) {
                 let count = ++countReviews;
 
-                if (count > window.scrollData.reviews.endPage) {
+                if (count >= window.scrollData.reviews.endPage) {
                     loader.classList.add(loaderHiddenClass);
                     return
                 }
@@ -76,61 +92,65 @@ const infiniteScroll = () => {
 
                 xhr.send(data);
 
-            }
+            }*/
 
-            if (type === 'articles') {
+            /*if (type === 'articles') {
                 let count = ++countArticles;
 
-                if (count > window.scrollData.articles.endPage) {
-                    loader.classList.add(loaderHiddenClass);
-                    return
+                if (count <= window.scrollData.articles.endPage) {
+                    loader.classList.remove(loaderHiddenClass);
+
+                    loadUrl = `${currentUrl}/index.php?PAGEN_${navNumArticles}=${count}`;
+
                 }
 
-                navNumArticles = window.scrollData.articles.loadSett.navNum;
-                loadUrl = `${currentUrl}/index.php?PAGEN_${navNumArticles}=${count}`;
-                xhr.open("POST", loadUrl);
+            }*/
 
-                xhr.onreadystatechange = function () {
-                    if (xhr.readyState === XMLHttpRequest.DONE) {
-                        let result = xhr.responseText;
-                        infinityContainer.insertAdjacentHTML('beforeend', result);
-                        loader.classList.add(loaderHiddenClass);
-                    }
-                };
+            const getUrl = (type) => {
 
-                xhr.send(data);
-            }
+                dataPagination[type].count = ++dataPagination[type].count;
 
-            if (type === 'default') {
-                let count = ++countDefault;
-
-                if (count > window.scrollData.default.endPage) {
-                    loader.classList.add(loaderHiddenClass);
-                    return
+                if(dataPagination[type].count <= dataPagination[type].endPage) {
+                    loader.classList.remove(loaderHiddenClass);
+                    return `${currentUrl}/index.php?PAGEN_${dataPagination[type].navNum}=${dataPagination[type].count}`;
                 }
+            };
 
-                navNumDefault = window.scrollData.default.loadSett.navNum;
-                loadUrl = `${currentUrl}/index.php?PAGEN_${navNumDefault}=${count}`;
-                xhr.open("POST", loadUrl);
+            let loadUrl = null;
 
-                xhr.onreadystatechange = function () {
-                    if (xhr.readyState === XMLHttpRequest.DONE) {
-                        let result = xhr.responseText;
-                        infinityContainer.insertAdjacentHTML('beforeend', result);
-                        loader.classList.add(loaderHiddenClass);
-                    }
-                };
-
-                xhr.send(data);
+            switch (type) {
+                case types.default:
+                    loadUrl = getUrl(type);
+                    break;
+                case types.articles:
+                    loadUrl = getUrl(type);
+                    break;
+                case types.reviews:
+                    loadUrl = getUrl(type);
+                    break;
+                default: break;
             }
 
+            if(loadUrl === null) {
+                return
+            }
+
+        xhr.open("POST", loadUrl);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                let result = xhr.responseText;
+                infinityContainer.insertAdjacentHTML('beforeend', result);
+                loader.classList.add(loaderHiddenClass);
+            }
+        };
+
+        xhr.send(data);
     };
-
 
     $(window).scroll(() => {
             if ($(window).scrollTop() + $(window).height() === $(document).height()) {
                 getData();
-                loader.classList.remove(loaderHiddenClass);
             }
 
         }
