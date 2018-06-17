448d3c808444c403df1ff6fa5fea870a41723fd5
prettyboyheaven <jemzetrim@gmail.com>
2018-06-17 11:07:45 +0300

    fix: error on main page

--- src/js/setMaterials.js
+++ src/js/setMaterials.js
@@ -1,6 +1,6 @@
 const setMaterials = ( {materialsVideoArr, materialsContentList, materialsVideoList, materials, materialsHiddenClass}) => {
 
-    if(materials === undefined) {
+    if(!materials) {
         return
     }
 
