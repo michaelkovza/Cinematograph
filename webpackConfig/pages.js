8518513a248e1a44b6a3c1119e285d7cc4de5d12
prettyboyheaven <jemzetrim@gmail.com>
2018-12-19 18:04:30 +0300

    fix: truncate titles

--- webpackConfig/pages.js
+++ webpackConfig/pages.js
@@ -10,182 +10,182 @@ module.exports = [
     },
     chunks: ['index']
   },
-  // {
-  //   title: '«materials»',
-  //   filename: 'materials.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'materials.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«author»',
-  //   filename: 'author.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'author.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«gallery»',
-  //   filename: 'gallery.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'gallery.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«gallery-detailed»',
-  //   filename: 'gallery-detailed.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'gallery-detailed.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«video»',
-  //   filename: 'video.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'video.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«video-detailed»',
-  //   filename: 'video-detailed.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'video-detailed.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«video-detailed-movies»',
-  //   filename: 'video-detailed-movies.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'video-detailed-movies.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«article»',
-  //   filename: 'article.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'article.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«article-simple»',
-  //   filename: 'simple-article.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'simple-article.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«album»',
-  //   filename: 'album.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'album.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«library»',
-  //   filename: 'library.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'library.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«book»',
-  //   filename: 'book.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'book.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«contacts»',
-  //   filename: 'contacts.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'contacts.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«search-page»',
-  //   filename: 'search-page.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'search-page.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«404»',
-  //   filename: '404.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: '404.pug'
-  //   },
-  //   chunks: ['index']
-  // },
-  // {
-  //   title: '«search-page-not-found»',
-  //   filename: 'search-page-not-found.html',
-  //   template: 'src/html/layouts/layout.pug',
-  //   inject: 'body',
-  //   minify: false,
-  //   env: {
-  //     page: 'search-page-not-found.pug'
-  //   },
-  //   chunks: ['index']
-  // }
+  {
+    title: '«materials»',
+    filename: 'materials.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'materials.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«author»',
+    filename: 'author.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'author.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«gallery»',
+    filename: 'gallery.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'gallery.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«gallery-detailed»',
+    filename: 'gallery-detailed.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'gallery-detailed.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«video»',
+    filename: 'video.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'video.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«video-detailed»',
+    filename: 'video-detailed.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'video-detailed.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«video-detailed-movies»',
+    filename: 'video-detailed-movies.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'video-detailed-movies.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«article»',
+    filename: 'article.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'article.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«article-simple»',
+    filename: 'simple-article.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'simple-article.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«album»',
+    filename: 'album.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'album.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«library»',
+    filename: 'library.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'library.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«book»',
+    filename: 'book.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'book.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«contacts»',
+    filename: 'contacts.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'contacts.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«search-page»',
+    filename: 'search-page.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'search-page.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«404»',
+    filename: '404.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: '404.pug'
+    },
+    chunks: ['index']
+  },
+  {
+    title: '«search-page-not-found»',
+    filename: 'search-page-not-found.html',
+    template: 'src/html/layouts/layout.pug',
+    inject: 'body',
+    minify: false,
+    env: {
+      page: 'search-page-not-found.pug'
+    },
+    chunks: ['index']
+  }
 
 ];
 
